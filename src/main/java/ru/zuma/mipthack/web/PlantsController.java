46aef9d886f388f5e9f55fa2327d1206a4cfaf3a
Stepan <stepannk@mail.ru>
2020-04-25 17:37:48 +0300

    different percentages 2

--- src/main/java/ru/zuma/mipthack/web/PlantsController.java
+++ src/main/java/ru/zuma/mipthack/web/PlantsController.java
@@ -19,21 +19,23 @@ public class PlantsController {
 
     @GetMapping("/")
     public ArrayList<PlantResponse> getPlants() {
-        ArrayList<PlantData> dataTime = new ArrayList<>();
-        dataTime.add(new PlantData(LocalDate.now(), 0));
-        dataTime.add(new PlantData(LocalDate.now(), 10));
-        dataTime.add(new PlantData(LocalDate.now(), 20));
-        dataTime.add(new PlantData(LocalDate.now().plusDays(1), 30));
-        dataTime.add(new PlantData(LocalDate.now().plusDays(1), 40));
-        dataTime.add(new PlantData(LocalDate.now().plusDays(2), 50));
-        dataTime.add(new PlantData(LocalDate.now().plusDays(2), 60));
-        dataTime.add(new PlantData(LocalDate.now().plusDays(3), 70));
-        dataTime.add(new PlantData(LocalDate.now().plusDays(3), 80));
-        dataTime.add(new PlantData(LocalDate.now().plusDays(3), 90));
-        dataTime.add(new PlantData(LocalDate.now().plusDays(3), 100));
+        ArrayList<PlantData> dataTime1 = new ArrayList<>();
+        dataTime1.add(new PlantData(LocalDate.now(), 0));
+        dataTime1.add(new PlantData(LocalDate.now().plusDays(1), 30));
+        dataTime1.add(new PlantData(LocalDate.now().plusDays(2), 50));
+        dataTime1.add(new PlantData(LocalDate.now().plusDays(3), 80));
+        dataTime1.add(new PlantData(LocalDate.now().plusDays(4), 90));
+
+        ArrayList<PlantData> dataTime2 = new ArrayList<>();
+        dataTime2.add(new PlantData(LocalDate.now(), 10));
+        dataTime2.add(new PlantData(LocalDate.now().plusDays(1), 40));
+        dataTime2.add(new PlantData(LocalDate.now().plusDays(2), 60));
+        dataTime2.add(new PlantData(LocalDate.now().plusDays(3), 70));
+        dataTime2.add(new PlantData(LocalDate.now().plusDays(4), 100));
 
         ArrayList<PlantResponse> plantResponses = new ArrayList<>();
-        plantResponses.add(new PlantResponse((long)0, "Конвертерный цех 1", "КЦ-1", dataTime));
+        plantResponses.add(new PlantResponse((long)0, "Конвертерный цех 1", "КЦ-1", dataTime1));
+        plantResponses.add(new PlantResponse((long)1, "Конвертерный цех 2", "КЦ-2", dataTime2));
 
         return plantResponses;
     }
